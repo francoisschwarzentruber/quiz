1d674ccefe912ca859f642315074c341b7b35a2d
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-03-08 19:13:55 +0100

    style

--- quiz.js
+++ quiz.js
@@ -4,23 +4,22 @@ function forEach(array, fn) {
     }
 }
 
-function installOnClick(answers, className) {
+
+window.onload = function () {
+    const answers = document.getElementsByTagName("answer");
     forEach(answers, (answer) => {
         answer.setAttribute("class", "answerable");
         answer.onclick = (evt) => {
             forEach(evt.target.parentNode.getElementsByTagName("answer"),
-                (a) => {a.setAttribute("class", "notanswered"); a.onclick = () => {};});
+                (a) => {
+                    a.setAttribute("class", "notanswered");
+                    a.onclick = () => { };
+                });
             evt.target.setAttribute("class", "answered");
-
             forEach(evt.target.parentNode.getElementsByTagName("explanation"),
                 (a) => a.setAttribute("class", "explanationVisible"));
         };
-
     });
-}
-
-window.onload = function () {
-    installOnClick(document.getElementsByTagName("answer"), "wrong");
 };
 
 
